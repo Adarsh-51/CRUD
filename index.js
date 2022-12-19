24080cf8ee450f1e5be5adafe283f5258222b093
Adarsh-51 <108488189+Adarsh-51@users.noreply.github.com>
2022-12-20 01:23:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+dotenv.config();
+
+
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log("connected to db")
+);
+
+
+const productRoutes = require("./routes/product");
+
+
+app.use(express.json());
+app.use(cors());
+
+
+app.use("/api/products", productRoutes);
+
+app.listen(4000, () => console.log("server up and runing on port 4000"));
